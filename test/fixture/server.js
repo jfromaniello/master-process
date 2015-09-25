f427f3c6e01b2de7c7afe2c80bd080655cf697fd
José F. Romaniello <jfromaniello@gmail.com>
2015-09-25 11:11:02 -0300

    minor

--- test/fixture/server.js
+++ test/fixture/server.js
@@ -13,16 +13,23 @@ if (process.env.RELOAD_WORKER) {
   worker_index = JSON.parse(process.env.RELOAD_WORKER).reload_count;
 }
 
-http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) {
   if (req.url === '/crash') {
     return process.exit(1);
   }
   res.writeHead(200);
   res.end(worker_index.toString());
-}).listen(9898, function (err) {
+});
+
+server.listen(9898, function (err) {
   if (err) {
     console.error(err);
     return process.exit(1);
   }
   console.log('listening');
+});
+
+process.once('SIGTERM', function () {
+  server.close();
+  process.exit(0);
 });
