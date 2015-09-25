618456ea5026f6aed3330654d8b53ec7b1ad88e0
José F. Romaniello <jfromaniello@gmail.com>
2015-09-25 09:43:57 -0300

    initial

---
+++ README.md
@@ -0,0 +1,67 @@
+The purpose of this module is to reload a node.js application with no downtime by using the [`cluster`](https://nodejs.org/api/cluster.html) capabilities.
+
+Read more [here](http://joseoncode.com/2015/01/18/reloading-node-with-no-downtime/).
+
+## Installation
+
+```
+npm i master-process --save
+```
+
+## Recommended usage
+
+Use this code at the very beginning of your node.js application:
+
+```js
+if (cluster.isMaster && 					//if is a master
+    typeof v8debug !== 'object' &&			//not in debug mode
+    process.env.NODE_ENV !== 'test') {      //not in test mode
+
+  var mp = require('master-process');
+  mp.init();
+  return;
+}
+```
+
+## How it works
+
+The application itself is run as a worker process.
+
+The master process handles the special `SIGHUP` signal to create a new worker and once the new worker is listening it closes the old one.
+
+Use this signal to tell the master process that you have updated the application and it should reload it.
+
+### Application Crashes
+
+The master-process does not handle application crashes and restarts. Once a worker crash the master process itself will crash, the service manager should take care of restarting the application.
+
+### Updating master-process
+
+If the master process detects that the version of the `master-process` module has changed it will quit with exit code 1. The service manager should take care of restarting the application.
+
+### CPU and Memory monitoring
+
+The master process watch by default the behavior of the worker. If the process is taking too much resources it will load a new worker.
+
+### SIGUSR2
+
+I use this special signal to profile the underlying application (check [v8profiler](https://github.com/node-inspector/v8-profiler)). The master process pauses-resume the CPU/Mem monitoring and pass the signal to the worker.
+
+### Unix sockets
+
+If `process.env.PORT` starts with an `/` (slash) master-process will assume you are going to listen on a unix socket and it will take care of few things:
+
+-  cleaning the socket if exists on start up, otherwise the worker will fail with EADDRINUSE.
+-  cleaning the socket on exit.
+
+## Debug
+
+Use `DEBUG=master-process` to debug this module.
+
+## Similar projects
+
+-  [cluster-master](https://github.com/isaacs/cluster-master).
+
+## License
+
+MIT 2015 - Jose F. Romaniello
---
+++ index.js
@@ -0,0 +1,109 @@
+var cluster = require('cluster');
+var path    = require('path');
+var fs      = require('fs');
+var async   = require('async');
+var _       = require('lodash');
+var monitor = require('./lib/monitor');
+var debug   = require('debug')('master-process');
+
+var cwd     = process.cwd();
+
+var reload_count = 0;
+
+function getVersion () {
+  var pkg = fs.readFileSync(path.join(__dirname, '/package.json'), 'utf8');
+  return JSON.parse(pkg).version;
+}
+
+var version = getVersion();
+
+function fork (callback) {
+  process.chdir(cwd);
+
+  if (version !== getVersion()) {
+    debug('master-process changed, restarting');
+    return process.exit(1);
+  }
+
+  debug('starting a new worker');
+
+  var reload_env = { RELOAD_WORKER: JSON.stringify({ reload_count: reload_count }) };
+  var new_worker = cluster.fork(reload_count > 0 ? reload_env : undefined);
+
+  reload_count++;
+
+  monitor(new_worker, debug, fork);
+
+  new_worker.once('listening', function () {
+    debug('new worker is listening');
+
+    _.values(cluster.workers)
+    .filter(function (worker) {
+      return worker.id !== new_worker.id;
+    })
+    .forEach(function (old_worker) {
+      var old_proc = old_worker.process;
+      debug('killing old worker with pid ' + old_proc.pid);
+      old_proc.kill('SIGTERM');
+    });
+
+    if (callback) {
+      callback(new_worker);
+    }
+  });
+}
+
+module.exports.init = function () {
+  // console.log('Starting master process with pid ' + process.pid);
+
+  debug('starting master-process with pid ' + process.pid);
+
+
+  if (process.env.PORT && process.env.PORT[0] === '/' && fs.existsSync(process.env.PORT)) {
+    fs.unlinkSync(process.env.PORT);
+  }
+
+  var unix_sockets = [];
+
+  process
+    .on('SIGHUP', fork)
+    .on('SIGTERM', function () {
+
+      debug('SIGTERM: stopping all workers');
+
+      async.each(_.values(cluster.workers), function (worker, callback) {
+        worker.process
+              .once('exit', function () {
+                callback();
+              })
+              .kill('SIGTERM');
+      }, function () {
+        unix_sockets.forEach(function (socket) {
+          debug('SIGTERM: cleaning socket ' + socket);
+          try {
+            fs.unlinkSync(socket);
+          } catch(er){}
+        });
+      });
+
+    }).on('SIGUSR2', function () {
+      debug('SIGUSR2: sending the signal to all workers');
+      _.values(cluster.workers).forEach(function (worker) {
+        worker.kill('SIGUSR2');
+      });
+    });
+
+  cluster.on('listening', function (worker, address) {
+    debug('cluster is listening');
+    if (address.address &&
+        address.address[0] === '/' ||
+        unix_sockets.indexOf(address.address) === -1) {
+      unix_sockets.push(address.address);
+    // fs.chmodSync(address.address, '777');
+    }
+  });
+
+  debug('forking the first workers');
+  //start the first child process
+  fork();
+};
